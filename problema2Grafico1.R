f556f769f1ec178891c21cb8a0b61802f088206e
Joao P. Targino <joaotargino@gmail.com>
2014-11-11 01:21:12 -0300

    mais dados

--- problema2Grafico1.R
+++ problema2Grafico1.R
@@ -1,4 +1,4 @@
-musicas <- read.csv('songdata.csv')
+musicas <- read.csv('data.csv')
 musicas$nomeMusica <- tolower(musicas$nomeMusica)
 
 titulos <- tolower(musicas$nomeMusica)
@@ -11,7 +11,7 @@ sorted.table<-paste(names(sorted.freq), sorted.freq, sep="\t")
 
 cat("Word\tFREQ", sorted.table, file="ordenadas", sep="\n")
 palavras <- read.table("ordenadas", sep="\t")
-#palavras pra analisar:
+#palavras pra analisar, por exemplo:
 #love, life, night, world,  heart, baby, little, good, girl, never, home, god
 
 #portugues/espanhol
@@ -22,6 +22,7 @@ palavras <- read.table("ordenadas", sep="\t")
 summary(musicas$tag[grep("you",musicas$nomeMusica)])
 summary(musicas$tag[grep("love",musicas$nomeMusica)])
 summary(musicas$tag[grep("life",musicas$nomeMusica)])
+summary(musicas$tag[grep("girl",musicas$nomeMusica)])
 
 
 you <- summary(musicas$tag[grep("you",musicas$nomeMusica)])
@@ -33,18 +34,21 @@ names(love)
 life <- c(summary(musicas$tag[grep("life",musicas$nomeMusica)]))
 names(life)
 
+girl <- c(summary(musicas$tag[grep("girl",musicas$nomeMusica)]))
+names(girl)
+
 
 # for ggplot2 0.92 release
 library(ggplot2)
 library(reshape)
 library(grid)
 
-dfpalavras <- structure(list(Temas = structure(c(2L, 3L, 1L), .Label = c("Life", "Love", "You"), class = "factor"), 
-                             Country = c(1, 8, 12), Jazz = c(0, 4, 4), Pop = c(2,18,20), Reggae= c(0,1,4), Rock = c(5, 20, 54)),
-                        .Names = c("Temas", "Country", "Jazz", "Pop", "Reggae", "Rock"), class = "data.frame", row.names = c(NA, -3L))
+dfpalavras <- structure(list(Temas = structure(c(4L, 2L, 3L, 1L), .Label = c("You", "Life", "Love", "Girl"), class = "factor"), 
+                             Electronica = c(1, 0, 0, 5), Jazz = c(0, 0, 4, 4), Pop = c(16, 5, 40, 59), Reggae= c(0, 2, 6, 13), Rock = c(16, 16, 72, 171)),
+                        .Names = c("Temas", "Electronica", "Jazz", "Pop", "Reggae", "Rock"), class = "data.frame", row.names = c(NA, -4L))
 dfm <- melt(dfpalavras, variable_name = "genero")
 
-levels(dfm$genero) <- c("Country", "Jazz", "Pop", "Reggae", "Rock")
+levels(dfm$genero) <- c("Electronica", "Jazz", "Pop", "Reggae", "Rock")
 p <- ggplot(dfm, aes(genero, value, group = Temas, colour = Temas))
 p1 <- p + geom_line(size = 1)
 
@@ -53,7 +57,7 @@ p2 <- p1 + theme_bw()
         panel.background = none, panel.border = none, axis.line = element_line(colour = "grey50"))
 p3 <- p2 + geom_vline( colour = "grey85", alpha = 0.5) + 
   geom_hline(yintercept = 32, colour = "grey80", alpha = 0.5) 
-p4 <- p3 + geom_text(data = dfm[dfm$genero == "Country", ], aes(label = Temas), hjust = 0.7, vjust = 1)
+p4 <- p3 + geom_text(data = dfm[dfm$genero == "Pop", ], aes(label = Temas), hjust = 0.7, vjust = 1)
 data_table <- ggplot(dfm, aes(x = genero, y = factor(Temas), label = format(value, nsmall = 1), colour = Temas)) 
 + geom_text(size = 3.5) + theme_bw() + scale_y_discrete(labels = abbreviate,
                                                         limits = c("Life", "Love", "You")) 
